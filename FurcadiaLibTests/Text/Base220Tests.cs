2519a18d98a36d75814afe14ab4134c422221092
Jeremy Trombley <gerolkae@hotmail.com>
2017-04-23 20:35:51 -0700

    Added ColorStrings parser and updated FURRE. FurreList, and Dream Classes to accomondate Created some Server To Client Instruction objects so they maybe delegated out as Server Events

---
+++ FurcadiaLibTests/Text/Base220Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using static Furcadia.Text.Base220;
+
+namespace FurcadiaLibTests.Text
+{
+    [TestFixture]
+    public class Base220Tests
+    {
+        #region Public Methods
+
+        [TestCase(@"/Hello world!", 12)]
+        [TestCase(@"/Hello world! Pft", 12)]
+        public void Base220StringLengeth_Returns_StringLengeth_And_Outputs_ReferenceString(string input, int ExpectedResult)
+        {
+            int length = Base220StringLengeth(ref input);
+        }
+
+        #endregion Public Methods
+    }
+}
