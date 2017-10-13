86445bc5647211ed0beece0bd8b3d052f7a17b5e
Jeremy Trombley <gerolkae@hotmail.com>
2017-10-13 08:18:30 -0700

    pft

---
+++ FurcadiaLibTests/Net/web/NetWebTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit;
+using NUnit.Framework;
+using Furcadia.Net.Web;
+using System.Collections.Generic;
+
+namespace FurcadiaLibTests.Net.web
+{
+    [TestFixture]
+    public class NetWebTests
+    {
+        [TestCase("http://silvermonkey.tsprojects.org")]
+        [TestCase("https://silvermonkey.tsprojects.org")]
+        public void TestPostDataIsNotNull(string url)
+        {
+          NetWeb  var = new NetWeb(url);
+            var list = var.PostData();
+            Assert.IsNotNull(var.PostData());
+
+        }
+
+    }
+}
