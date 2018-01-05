9ce04935d8fd04b7a22e904d5f112245ea4add84
Jeremy Trombley <gerolkae@hotmail.com>
2018-01-05 09:33:33 -0700

    Add tests for Furre and Dream Objects

---
+++ FurcadiaLibTests/Net/Dream/DreamTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Furcadia.Net.DreamInfo;
+
+namespace FurcadiaLibTests.Net.Dreams
+{
+    [TestFixture]
+    public class DreamTests
+    {
+        [Test]
+        public void DreamIsNullcheck()
+        {
+            Furcadia.Net.DreamInfo.Dream dream = null;
+            Assert.Multiple(() =>
+            {
+                Assert.That(dream, Is.EqualTo(null));
+                dream = new Furcadia.Net.DreamInfo.Dream();
+                Assert.That(dream, !Is.EqualTo(null));
+            });
+        }
+    }
+}
---
+++ FurcadiaLibTests/Net/Dream/FurreTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Furcadia.Net.DreamInfo;
+using NUnit.Framework;
+
+namespace FurcadiaLibTests.Net.Dream
+{
+    [TestFixture]
+    public class FurreTests
+    {
+        [Test]
+        public void FurreIsNull()
+        {
+            Furre Furre1 = null;
+            Assert.Multiple(() =>
+            {
+                Assert.That(Furre1, Is.EqualTo(null));
+                Furre1 = new Furre();
+                Assert.That(Furre1, !Is.EqualTo(null));
+            });
+        }
+    }
+}
